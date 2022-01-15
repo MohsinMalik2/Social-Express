a8e6ce3567155c40bb3c5afd6860811a2eb1b69c
MohsinMalik2 <malikmuhsin70@gmail.com>
2022-01-15 21:07:56 +0500

    preloader

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 import News from './components/News'
-
 export default class App extends Component {
   // c = "John";
   render() {
     return (
       <div>
         {/* This is my first Class Based COmponent {this.c} */}
+        
         <NavBar/>
         <News/>
         <Footer/>
--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import Spinner from './Spinner'
 
 class News extends Component {
 
@@ -8,37 +9,54 @@ class News extends Component {
             this.state = {
                 articles: [],
                 loading: false, 
-                page: 1
+                page: 1,
+                pageSize:15,
+                category: "business" 
 
             }
         }
         async componentDidMount(){
-            var url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b7e3db46081440d5b4c54a42081743c6&pageSize=12&page=1";
+            var url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.state.category}&apiKey=b7e3db46081440d5b4c54a42081743c6&pageSize=${this.state.pageSize}&page=1`;
+            this.setState({
+                loading : true
+            });  
             var data = await fetch(url);
             var parseData = await data.json();
             this.setState({
                 articles: parseData.articles,
                 totalResult: parseData.totalResults,
-                page: 1
+                page: 1,
+                loading : false
             });
         }
         prevFunction = async () =>{
-            var url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b7e3db46081440d5b4c54a42081743c6&pageSize=12&page=${this.state.page-1}`;
+            var url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.state.category}&apiKey=b7e3db46081440d5b4c54a42081743c6&pageSize=${this.state.pageSize}&page=${this.state.page-1}`;
+            this.setState({
+                loading : true
+            }); 
             var data = await fetch(url);
+           
             var parseData = await data.json();
             this.setState({
                 articles: parseData.articles,
-                page: this.state.page - 1
+                page: this.state.page - 1,
+                category: 'sports',
+                loading: false 
 
             });
         };
         nextFunction = async () =>{
-            var url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b7e3db46081440d5b4c54a42081743c6&pageSize=12&page=${this.state.page-1}`;
+            var url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.state.category}&apiKey=b7e3db46081440d5b4c54a42081743c6&pageSize=${this.state.pageSize}&page=${this.state.page+1 }`;
+            this.setState({
+                loading : true
+            });  
+
             var data = await fetch(url);
+           
             var parseData = await data.json();
             if( Math.ceil(this.state.totalResult/12) < this.state.page +1){
 
-                alert("Pages End Here");
+                
             }
             else{
 
@@ -46,7 +64,9 @@ class News extends Component {
                     
                     articles: parseData.articles,
                     totalResult: parseData.totalResults,
-                    page: this.state.page + 1
+                    page: this.state.page + 1,
+                    category: 'technology',
+                    loading:  false 
                 });
             }
             
@@ -54,11 +74,13 @@ class News extends Component {
     render() {
         return (
             <div className='container my-3'>
+
                 <div className="row">
                     <div className="col-md-12 mb-4">
                         <h1>Social Express - Top Headlines</h1>
                     </div>
-                    {this.state.articles.map((element)=>{
+                    {this.state.loading &&<Spinner />}
+                    {!this.state.loading && this.state.articles.map((element)=>{
                            return <div className='col-md-4 text-center'>
                                     <NewsItem newsUrl={element.url} key={element.url} title={element.title?element.title.slice(0,45):""} desc={element.description?element.description.slice(0,88):""} img={element.urlToImage?element.urlToImage:"https://cdn.vox-cdn.com/thumbor/4ECl1MgKXJDJ_pRPP6p4u_NPCaQ=/0x434:6056x3605/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22964001/verge_vjeran_pavic_meta_3_20211028.jpg"}/>
                                </div>
@@ -68,7 +90,7 @@ class News extends Component {
                         <button className="btn btn-primary mx-1" disabled={this.state.page<=1} onClick={this.prevFunction} > &larr; Previous</button>
                             <button className="btn btn-primary mx-1" > {this.state.page} </button>
 
-                        <button className="btn btn-primary mx-1 des" onClick={this.nextFunction} >Next &rarr;</button>
+                        <button disabled={Math.ceil(this.state.totalResult/12) < this.state.page +1} className="btn btn-primary mx-1 des" onClick={this.nextFunction} >Next &rarr;</button>
                     </div>
                 </div>
             </div>
